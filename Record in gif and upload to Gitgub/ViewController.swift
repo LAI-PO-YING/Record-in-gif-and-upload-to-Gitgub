852acd6e3ca4106b1973bf9314ca8d8660cd12c7
Ivan Lai <229705jack2gmail.com>
2021-03-10 12:36:23 +0800

    Initial Commit

---
+++ Record in gif and upload to Gitgub/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Record in gif and upload to Gitgub
+//
+//  Created by Lai Po Ying on 2021/3/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
